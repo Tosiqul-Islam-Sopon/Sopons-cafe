dbce4489f86feba79cf92a3b4afa26ed62394c50
Tosiqul Islam <sopon.tosiq@gmail.com>
2024-03-15 22:32:02 +0600

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-4xl'>Vite + React</h1>
-      
+      {/* <h1 classNameName='text-4xl'>Vite + React</h1> */}
+      <Navbar></Navbar>
+
     </>
   )
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import img1 from '../../assets/Images/Frame 5.png'
+import search from '../../assets/Images/search.png'
+import '../../index.css'
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between items-center lexend'>
+            <div>
+                <h3 className='text-3xl font-bold '>Recipe Calories</h3>
+            </div>
+            <div className='flex justify-between gap-4'>
+                <p>Home</p>
+                <p>Recipes</p>
+                <p>About</p>
+                <p>Search</p>
+            </div>
+            <div className='flex gap-3 '>
+                
+                <div className='flex items-center bg-[#150B2B0D] gap-2 p-3 rounded-xl'>
+                    <img src={search} alt="" />
+                    <input className='p-1 rounded-xl' type="text" placeholder='Search' />
+                </div>
+                <img src={img1} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
