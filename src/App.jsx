b93d076702f6718ec964c1c5629c0be38b3998a4
Tosiqul Islam <sopon.tosiq@gmail.com>
2024-03-15 23:00:13 +0600

    banner done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 
 import './App.css'
+import Banner from './Components/Banner/Banner'
 import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
-    <>
+    <div className='space-y-10 my-10 mx-14'>
       {/* <h1 classNameName='text-4xl'>Vite + React</h1> */}
       <Navbar></Navbar>
-
-    </>
+      <Banner></Banner>
+    </div>
   )
 }
 
---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,21 @@
+// import banner from '../../assets/Images/banner.png'
+import './Banner.css'
+const Banner = () => {
+    return (
+        <div className='banner lexend'>
+            <div className="p-24 space-y-8">
+                <div>
+                <h2 className='text-white text-4xl font-bold text-center'>Discover an exceptional cooking</h2>
+                <h2 className='text-white text-4xl font-bold text-center'>class tailored for you!</h2>
+                </div>
+                <p className='text-white text-center'>Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding problems to become an exceptionally well world-class Programmer.</p>
+                <div className='flex gap-5 items-center justify-center'>
+                    <button className='bg-[#0BE58A] p-3 rounded-xl font-medium'>Explore Now</button>
+                    <button className='text-white font-medium bg-transparent border border-white p-3 rounded-xl'>Our Feedback</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
